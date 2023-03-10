e9181826efff8f4e64249b91d7e2c91721147260
MAHENDRA SAI DIVI <92313437+MAHENDRA9535@users.noreply.github.com>
2023-03-10 14:54:13 +0530

    Add files via upload

---
+++ word_replacement/word_replace.py
@@ -0,0 +1,8 @@
+def wordreplacement():
+    str = input("Enter the string of you choice:")
+    word_to_be_replaced = input("enter the string to be replaced: ")
+    word_replacement = input("enter the word replacement")
+    print(str.replace(word_to_be_replaced, word_replacement))
+
+
+wordreplacement()
