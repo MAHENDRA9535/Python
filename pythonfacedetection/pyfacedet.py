1787cbfd76350f21f71e57bb62d85dda134b294e
MAHENDRA SAI DIVI <92313437+MAHENDRA9535@users.noreply.github.com>
2023-03-17 18:23:17 +0530

    Update pyfacedet.py

--- pythonfacedetection/pyfacedet.py
+++ pythonfacedetection/pyfacedet.py
@@ -1,9 +1,9 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
 
 img = cv2.imread(
-    "C:\all files\projects\python\20 projects\python face detection\est1.jpg")
+    "python face detection\est1.jpg")
 
 if (img is not None):
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -14,6 +14,6 @@ for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (225, 0, 0), 2)
 
 cv2.imshow("img", img)
-cv2.waitkey()
+cv2.waitKey()
 
 cv2.imwrite("face_detected.jpg", img)
